70a104dd689658aee1bc14ef6b7f5b7302c02b7c
Michael Muehlemann <m.muehlemann@gmx.net>
2024-07-25 14:27:50 +0200

    Const Arrays for LED Patterns

---
+++ Firmware/wordclock/wordclock.ino
@@ -0,0 +1,39 @@
+
+
+// LED Patterns for the different Words
+const int ES[]      = {99, 109};
+const int ISCH[]    = {79, 69, 59, 49};
+const int FUEF[]    = {29, 19, 9};
+const int ZAEAE[]   = {28, 18, 8};
+const int VIURTU[]  = {108, 98, 88, 78, 68, 58};
+const int ZWAENZG[] = {107, 97, 87, 77, 67, 57};
+const int VOR[]     = {27, 17, 7};
+const int AB[]      = {76, 66, 56, 46, 36};
+const int EIS[]     = {105, 95, 85};
+const int ZWOEI[]   = {75, 65, 55, 45};
+const int DRUE[]    = {25, 15, 5};
+const int VIERI[]   = {104, 94, 84, 74, 64};
+const int FUEFI[]   = {54, 44, 34, 24};
+const int SAECHSI[] = {103, 93, 83, 73, 63, 53};
+const int SIEBNI[]  = {43, 33, 23, 13, 3};
+const int ACHTI[]   = {102, 92, 82, 72, 62};
+const int NUENI[]   = {52, 42, 32, 22};
+const int ZAEHNI[]  = {101, 91, 81, 71};
+const int EUFI[]    = {31, 21, 11, 1};
+const int ZWOEUFI[] = {100, 90, 80, 70, 60, 50};
+
+// LED Patterns for Minutes in Corners
+const int MIN_ONE[]   = {111};
+const int MIN_TWO[]   = {111, 110};
+const int MIN_THREE[] = {111, 110, 112};
+const int MIN_FOUR[]  = {111, 110, 112, 113};
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

5957ca914b431c73a67af5e2f11024426b05b69e
michael <m.muehlemann@gmx.net>
2024-12-22 21:01:35 +0100

    First Draft getting time arrays

--- Firmware/wordclock/wordclock.ino
+++ Firmware/wordclock/wordclock.ino
@@ -26,14 +26,14 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
 
 
 // LED Patterns for the different Words
-const int ES[]      = {90, 109};
-const int ISCH[]    = {70, 69, 50, 49};
+const int ESISCH[]  = {90, 109, 70, 69, 50, 49};
 const int FUEF[]    = {29, 10, 9};
 const int ZAEAE[]   = {28, 11, 8};
 const int VIURTU[]  = {108, 91, 88, 71, 68, 51};
 const int ZWAENZG[] = {107, 92, 87, 72, 67, 52};
 const int VOR[]     = {27, 12, 7};
 const int AB[]      = {73, 66, 53, 46, 33};
+const int HAUBI[]   = {86, 73, 66, 53, 46, 33};
 const int EIS[]     = {105, 94, 85};
 const int ZWOEI[]   = {74, 65, 54, 45};
 const int DRUE[]    = {25, 14, 5};
@@ -53,7 +53,8 @@ const int MIN_TWO[]   = {111, 110};
 const int MIN_THREE[] = {111, 110, 112};
 const int MIN_FOUR[]  = {111, 110, 112, 113};
 
-const int *Test [] = {ES, ISCH, FUEF, AB, ZAEHNI};
+const int *Test[] = {ES, ISCH, FUEF, AB, ZAEHNI};
+const int Test2[] = {ES, ISCH, FUEF, AB, ZAEHNI};
 
 void setup() {
   // Setup LEDs
@@ -63,17 +64,113 @@ void setup() {
   strip.setBrightness(BRIGHTNESS);
   Serial.println("Setup Complete");
 
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  for (int i = 0; i < sizeof(Test) / sizeof(Test[0]); i++) {
+    showTime(strip.Color(255, 0, 0, 0), Test[i], sizeof(Test[i]) / sizeof(Test[i][0])); // Red
+    delay(1000);
+  }
+  //showTime(strip.Color(  0,   0,   0, 255), EIS, sizeof(EIS) / sizeof(EIS[0])); // True white (not RGB white)
 
-  showTime(strip.Color(  0,   0,   0, 255), EIS, sizeof(EIS) / sizeof(EIS[0])); // True white (not RGB white)
 
 
+}
 
+*int[], *int[], *int[] build_time_array(int hour, int minute) {
+  *int intro[] = {ES, ISCH};
+  *int minutes[];
+  *int hours[];
+
+  // Get "Minutes" Text
+  switch (minute/5)
+  {
+  case 1:
+    minutes = {FUEF, AB};
+    break;
+  case 2:
+    minutes = {ZAEAE, AB};
+    break;
+  case 3:
+    minutes = {VIURTU, AB};
+    break;
+  case 4: 
+    minutes = {ZWAENZG, AB};
+    break;  
+  case 5:
+    minutes = {FUEF, VOR, HAUBI};
+    break;
+  case 6:
+    minutes = {HAUBI};
+    break;
+  case 7:
+    minutes = {FUEF, AB, HAUBI};
+    break; 
+  case 8: 
+    minutes = {ZWAENZG, VOR};
+    break;
+  case 9:
+    minutes = {VIURTU, VOR}; 
+    break;
+  case 10:  
+    minutes = {ZAEAE, VOR};
+    break;  
+  case 11:  
+    minutes = {FUEF, VOR};
+    break;
+  default:
+    break;
+  }
+
+  // Get "Hours" Text
+  // Add 1 to hour if minute is greater than 25 ("5 vor Haubi XX")
+  if minute >= 25 {
+    hour++;
+  }
+
+  switch (hour%12)
+  {
+  case 1:
+    hours = {EIS};
+    break;
+  case 2:
+    hours = {ZWOEI};
+    break;
+  case 3:
+    hours = {DRUE};
+    break;  
+  case 4:
+    hours = {VIERI};
+    break;
+  case 5:
+    hours = {FUEFI};
+    break;
+  case 6:
+    hours = {SAECHSI};
+    break;  
+  case 7:
+    hours = {SIEBNI};
+    break;
+  case 8:
+    hours = {ACHTI};
+    break;
+  case 9: 
+    hours = {NUENI};
+    break;
+  case 10:
+    hours = {ZAEHNI};
+    break;
+  case 11:
+    hours = {EUFI};
+    break;
+  case 12:
+    hours = {ZWOEUFI};
+    break;
+  }
+
+  return {intro, minutes, hours};
 }
 
 void showTime(uint32_t color, const int pixels[], int size) {

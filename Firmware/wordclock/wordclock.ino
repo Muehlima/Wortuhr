10e418f954d5e8c7882b19437f37100459604a75
michael <m.muehlemann@gmx.net>
2024-12-24 08:23:04 +0100

    change to struct, debugging, current state

--- Firmware/wordclock/wordclock.ino
+++ Firmware/wordclock/wordclock.ino
@@ -8,7 +8,7 @@
 #define LED_PIN    D5
 
 // How many NeoPixels are attached to the Arduino?
-#define LED_COUNT  110
+#define LED_COUNT  110  // Max Value 255 (uint8_t)
 
 // NeoPixel brightness, 0 (min) to 255 (max)
 #define BRIGHTNESS 50 // Set BRIGHTNESS to about 1/5 (max = 255)
@@ -24,37 +24,40 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 
+// Struct for LED Patterns
+typedef struct {
+  size_t size;
+  uint8_t word[];
+} Word;
 
 // LED Patterns for the different Words
-const int ESISCH[]  = {90, 109, 70, 69, 50, 49};
-const int FUEF[]    = {29, 10, 9};
-const int ZAEAE[]   = {28, 11, 8};
-const int VIURTU[]  = {108, 91, 88, 71, 68, 51};
-const int ZWAENZG[] = {107, 92, 87, 72, 67, 52};
-const int VOR[]     = {27, 12, 7};
-const int AB[]      = {73, 66, 53, 46, 33};
-const int HAUBI[]   = {86, 73, 66, 53, 46, 33};
-const int EIS[]     = {105, 94, 85};
-const int ZWOEI[]   = {74, 65, 54, 45};
-const int DRUE[]    = {25, 14, 5};
-const int VIERI[]   = {104, 95, 84, 74, 64};
-const int FUEFI[]   = {55, 44, 35, 24};
-const int SAECHSI[] = {103, 96, 83, 76, 63, 56};
-const int SIEBNI[]  = {43, 36, 23, 16, 3};
-const int ACHTI[]   = {102, 97, 82, 77, 62};
-const int NUENI[]   = {57, 42, 37, 22};
-const int ZAEHNI[]  = {101, 98, 81, 78};
-const int EUFI[]    = {38, 21, 18, 1};
-const int ZWOEUFI[] = {100, 99, 80, 79, 60, 59};
+const Word ESISCH = {6, {90, 109, 70, 69, 50, 49}};
+const Word FUEF = {3, {29, 10, 9}};
+const Word ZAEAE = {3, {28, 11, 8}};
+const Word VIURTU = {6, {108, 91, 88, 71, 68, 51}};
+const Word ZWAENZG = {6, {107, 92, 87, 72, 67, 52}};
+const Word AB = {2, {106, 93}};
+const Word VOR = {3, {27, 12, 7}};
+const Word HAUBI = {6, {86, 73, 66, 53, 46, 33}};
+const Word EIS = {3, {105, 94, 85}};
+const Word ZWOEI = {4, {74, 65, 54, 45}};
+const Word DRUE = {3, {25, 14, 5}};
+const Word VIERI = {5, {104, 95, 84, 74, 64}};
+const Word FUEFI = {4, {55, 44, 35, 24}};
+const Word SAECHSI = {6, {103, 96, 83, 76, 63, 56}};
+const Word SIEBNI = {5, {43, 36, 23, 16, 3}};
+const Word ACHTI = {5, {102, 97, 82, 77, 62}};
+const Word NUENI = {4, {57, 42, 37, 22}};
+const Word ZAENI = {4, {101, 98, 81, 78}};
+const Word EUFI = {4, {38, 21, 18, 1}};
+const Word ZWOEUFI = {6, {100, 99, 80, 79, 60, 59}};
 
 // LED Patterns for Minutes in Corners
-const int MIN_ONE[]   = {111};
-const int MIN_TWO[]   = {111, 110};
-const int MIN_THREE[] = {111, 110, 112};
-const int MIN_FOUR[]  = {111, 110, 112, 113};
+const Word MIN_ONE = {1, {111}};
+const Word MIN_TWO = {2, {111, 110}};
+const Word MIN_THREE = {3, {111, 110, 112}};
+const Word MIN_FOUR = {4, {111, 110, 112, 113}};
 
-const int *Test[] = {ES, ISCH, FUEF, AB, ZAEHNI};
-const int Test2[] = {ES, ISCH, FUEF, AB, ZAEHNI};
 
 void setup() {
   // Setup LEDs
@@ -69,56 +72,79 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  for (int i = 0; i < sizeof(Test) / sizeof(Test[0]); i++) {
-    showTime(strip.Color(255, 0, 0, 0), Test[i], sizeof(Test[i]) / sizeof(Test[i][0])); // Red
-    delay(1000);
-  }
+  // for (int i = 0; i < sizeof(Test) / sizeof(Test[0]); i++) {
+  //   showTime(strip.Color(255, 0, 0, 0), Test[i], sizeof(Test[i]) / sizeof(Test[i][0])); // Red
+  //   delay(1000);
+  // }
   //showTime(strip.Color(  0,   0,   0, 255), EIS, sizeof(EIS) / sizeof(EIS[0])); // True white (not RGB white)
 
+  //Serial.println(Test);
+  //Serial.println(Test2);
 
+  int currentHour = 10; // Beispielwerte
+  int currentMinute = 23;
 
+  Word timeArray = build_time_struct(currentHour, currentMinute);
+
+  // Zeige die Zeit an
+  showTime(strip.Color(0, 0, 0, 255), timeArray.word, timeArray.size); // True white (not RGB white)
+
+  delay(10000); // Wartezeit (1 Sekunde)
 }
 
-*int[], *int[], *int[] build_time_array(int hour, int minute) {
-  *int intro[] = {ES, ISCH};
-  *int minutes[];
-  *int hours[];
+Word build_time_struct(int hour, int minute)
+{
+  Word* wordArray = (Word*)malloc(10 * sizeof(Word)); // Platz für bis zu 10 Wörter (anpassbar)
+  int wordCount = 0;         // Anzahl der Wörter
+  wordArray[wordCount++] = ESISCH;
 
   // Get "Minutes" Text
-  switch (minute/5)
+  switch (minute / 5)
   {
   case 1:
-    minutes = {FUEF, AB};
+    wordArray[wordCount++] = FUEF;
+    wordArray[wordCount++] = AB;
     break;
   case 2:
-    minutes = {ZAEAE, AB};
+    wordArray[wordCount++] = ZAEAE;
+    wordArray[wordCount++] = AB;
     break;
   case 3:
-    minutes = {VIURTU, AB};
+    wordArray[wordCount++] = VIURTU;
+    wordArray[wordCount++] = AB;
     break;
-  case 4: 
-    minutes = {ZWAENZG, AB};
-    break;  
+  case 4:
+    wordArray[wordCount++] = ZWAENZG;
+    wordArray[wordCount++] = AB;
+    break;
   case 5:
-    minutes = {FUEF, VOR, HAUBI};
+    wordArray[wordCount++] = FUEF;
+    wordArray[wordCount++] = VOR;
+    wordArray[wordCount++] = HAUBI;
     break;
   case 6:
-    minutes = {HAUBI};
+    wordArray[wordCount++] = HAUBI;
     break;
   case 7:
-    minutes = {FUEF, AB, HAUBI};
-    break; 
-  case 8: 
-    minutes = {ZWAENZG, VOR};
+    wordArray[wordCount++] = FUEF;
+    wordArray[wordCount++] = AB;
+    wordArray[wordCount++] = HAUBI;
+    break;
+  case 8:
+    wordArray[wordCount++] = ZWAENZG;
+    wordArray[wordCount++] = VOR;
     break;
   case 9:
-    minutes = {VIURTU, VOR}; 
+    wordArray[wordCount++] = VIURTU;
+    wordArray[wordCount++] = VOR;
     break;
-  case 10:  
-    minutes = {ZAEAE, VOR};
-    break;  
-  case 11:  
-    minutes = {FUEF, VOR};
+  case 10:
+    wordArray[wordCount++] = ZAEAE;
+    wordArray[wordCount++] = VOR;
+    break;
+  case 11:
+    wordArray[wordCount++] = FUEF;
+    wordArray[wordCount++] = VOR;
     break;
   default:
     break;
@@ -126,54 +152,79 @@ void loop() {
 
   // Get "Hours" Text
   // Add 1 to hour if minute is greater than 25 ("5 vor Haubi XX")
-  if minute >= 25 {
+  if (minute >= 25) {
     hour++;
   }
 
-  switch (hour%12)
+  switch (hour % 12)
   {
   case 1:
-    hours = {EIS};
+    wordArray[wordCount++] = EIS;
     break;
   case 2:
-    hours = {ZWOEI};
+    wordArray[wordCount++] = ZWOEI;
     break;
   case 3:
-    hours = {DRUE};
-    break;  
+    wordArray[wordCount++] = DRUE;
+    break;
   case 4:
-    hours = {VIERI};
+    wordArray[wordCount++] = VIERI;
     break;
   case 5:
-    hours = {FUEFI};
+    wordArray[wordCount++] = FUEFI;
     break;
   case 6:
-    hours = {SAECHSI};
-    break;  
+    wordArray[wordCount++] = SAECHSI;
+    break;
   case 7:
-    hours = {SIEBNI};
+    wordArray[wordCount++] = SIEBNI;
     break;
   case 8:
-    hours = {ACHTI};
+    wordArray[wordCount++] = ACHTI;
     break;
-  case 9: 
-    hours = {NUENI};
+  case 9:
+    wordArray[wordCount++] = NUENI;
     break;
   case 10:
-    hours = {ZAEHNI};
+    wordArray[wordCount++] = ZAENI;
     break;
   case 11:
-    hours = {EUFI};
+    wordArray[wordCount++] = EUFI;
     break;
   case 12:
-    hours = {ZWOEUFI};
+    wordArray[wordCount++] = ZWOEUFI;
     break;
   }
 
-  return {intro, minutes, hours};
+  // Inhalte der Wörter in das finale Array kopieren
+  // Get Size of the final Array
+  size_t totalSize = 0;
+  for (int i = 0; i < wordCount; i++)
+  {
+    totalSize += wordArray[i].size;
+  }
+
+  Word time_struct;
+  time_struct.size = totalSize;
+
+
+
+  int bufferSize = 0;
+  for (int i = 0; i < wordCount; i++)
+  {
+    for (int j = 0; j < wordArray[i].size; j++)
+    {
+      time_struct.word[bufferSize++] = wordArray[i].word[j];
+    }
+  }
+
+  free(wordArray); // Free allocated memory
+  return time_struct;
+  
+  
 }
 
-void showTime(uint32_t color, const int pixels[], int size) {
+void showTime(uint32_t color, uint8_t pixels[], int size) {
   for (int i = 0; i < size; i++) {
     strip.setPixelColor(pixels[i], color);         //  Set pixel's color (in RAM)
     strip.show();                          //  Update strip to match
